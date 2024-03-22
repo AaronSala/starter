8174a5e820e84ea74394dc1524d95fe8a1d16347
Aaron Sala <salaaron2@gmail.com>
2024-03-22 12:41:04 +0300

    more of react list

--- src/App.js
+++ src/App.js
@@ -1,22 +1,14 @@
 
-import './App.css';
-
-
+import Garage from './inames.jsx'
 
 
 function App() {
-  const title = "this the first blog"
-   
-     const math = Math.random() * 20;
-   
+  
   return (
-    <div className="App">
-      <div className="content">
-        <h1>{title}</h1>
-           {math}
-        <p>{Math.ceil(math)}</p>
-      </div>
-    </div>
+    <>
+  
+      <Garage  />
+    </>
   );
 }
 
